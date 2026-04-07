6e3fa397a8b9c7fb13a0f8feca5d16b04f0e170d
Markus Borris <markus.borris@check24.de>
2026-04-07 12:18:03 +0200

    Initial import from cpp-lib-template

---
+++ src/alloc-statistics.cpp
@@ -0,0 +1,7 @@
+#include "mb/alloc-statistics/alloc-statistics.hpp"
+
+namespace mb::alloc_statistics {
+int32_t sum(int32_t a, int32_t b) {
+    return a + b;
+}
+} // namespace mb::alloc_statistics
